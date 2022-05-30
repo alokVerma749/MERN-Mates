ff9dd0d6cc46600e8d7bbee7089b61a7a1778257
Nitin Mishra <mnitin2311@gmail.com>
2022-05-30 20:25:46 +0530

    logo changes

--- frontend/src/Users/pages/Users.js
+++ frontend/src/Users/pages/Users.js
@@ -1,58 +1,50 @@
-import React from 'react'
-import UserList from "../components/UserList"
+import React from "react";
+import UserList from "../components/UserList";
 
 function Users() {
-
   // This is a dummy data, later these data will be fetched from Mongodb database.
 
-    const USERS = [
-        {
-            id:"u1",
-            name:"Alok",
-            image:"https://picsum.photos/200",
-            blogs:3
-        },
-        {
-            id:"u2",
-            name:"Tuzun",
-            image:"https://picsum.photos/200",
-            blogs:4
-        },
-        {
-          id:"u3",
-          name:"Nitin",
-          image:"https://picsum.photos/200",
-          blogs:10
-        },
-        {
-          id:"u4",
-          name:"Yasir Arafat",
-          image:"https://picsum.photos/200",
-          blogs:8
-        },
-        {
-          id:"u5",
-          name:"vinod sharma",
-          image:"https://picsum.photos/200",
-          blogs:1
-        },
-        {
-          id:"u6",
-          name:"Muhammad Zohaib Arshad",
-          image:"https://picsum.photos/200",
-          blogs:2
-        },
-        {
-          id:"u7",
-          name:"G.Vageesha Datta",
-          image:"https://picsum.photos/200",
-          blogs:7
-        } 
-    ]
+  const USERS = [
+    {
+      id: "u1",
+      name: "Alok",
+      image: "https://picsum.photos/200",
+      blogs: 3,
+    },
+    {
+      id: "u2",
+      name: "Anurag",
+      image: "https://picsum.photos/200",
+      blogs: 4,
+    },
+    {
+      id: "u3",
+      name: "Nitin Mishra",
+      image: "https://picsum.photos/200",
+      blogs: 10,
+    },
+    {
+      id: "u4",
+      name: "Bhupen Pandav",
+      image: "https://picsum.photos/200",
+      blogs: 8,
+    },
+    {
+      id: "u5",
+      name: "vinod sharma",
+      image: "https://picsum.photos/200",
+      blogs: 1,
+    },
 
-  return (
-    <UserList items= {USERS}/>
-  )
+    {
+      id: "u7",
+      name: "G.Vageesha Datta",
+      image: "https://picsum.photos/200",
+      blogs: 7,
+    },
+  ];
+
+  return <UserList items={USERS} />;
 }
 
 export default Users;
--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
-import SideDrawer from './SideDrawer';
+import SideDrawer from "./SideDrawer";
 import NavLinks from "./NavLinks";
 import Backdrop from "../UIElements/Backdrop";
 
@@ -13,40 +13,42 @@ const MainNavigation = () => {
 
   const openDrawer = () => {
     setDrawerIsOpen(true);
-  }
+  };
   const closeDrawer = () => {
     setDrawerIsOpen(false);
-  }
+  };
   return (
     <React.Fragment>
-      
       {/* This is the code for drawer button for the navigation on small screens */}
 
       {/* If drawer is open then the <Backdrop/> is rendered */}
-      {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
 
-      { drawerIsOpen &&
-      (<SideDrawer>
-      <nav className='main-navigation__drawer-nav'>
-        <NavLinks />
-      </nav>
-    </SideDrawer>)} 
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
 
-    <MainHeader>
-      <button className="main-navigation__menu-btn" onClick={openDrawer}>
-            <span />
-            <span />
-            <span />
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
         </button>
         <h1 className="main-navigation__title">
-            <Link to="/">MERN-Mates</Link> 
+          <Link to="/">
+            <img src={"/logo.png"} alt="logo" />
+          </Link>
         </h1>
         <nav className="main-navigation__header-nav">
-            <NavLinks/>
+          <NavLinks />
         </nav>
-    </MainHeader>
+      </MainHeader>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default MainNavigation;
